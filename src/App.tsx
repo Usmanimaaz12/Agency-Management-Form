f3e4d31333fcede1c2a14abefb37d1f90859dae4
Maaz Usmani <maaz.usmani@squareboat.com>
2026-04-18 16:57:59 +0530

    ADD: reusable button component

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import "./App.css";
+import Button from "./components/ui/Button";
 
 function App() {
   return (
     <>
-      <h1 className="w-full bg-black text-white">Agency Management</h1>
+      <h1 className="w-full bg-black text-white">Agency Management</h1>/
+      <Button variant="dark" className="rounded-sm"> test Button</Button>
     </>
   );
 }
---
+++ src/components/ui/Button.tsx
@@ -0,0 +1,35 @@
+import React from 'react'
+import { cn } from '../../libs/utils/common'
+
+// button prop type
+type ButtonProps = {
+    variant?: "primary" | "secondary" | "success" | "danger" | "warning" | "info" | "light" | "dark",
+    size?: "small" | "medium" | "large",
+    className?: string,
+    children: React.ReactNode,
+} & React.ButtonHTMLAttributes<HTMLButtonElement>
+
+const Button = ({variant = "primary", size = "medium", className, children, ...props}: ButtonProps) => {
+    const variants = {
+        primary: "bg-blue-500 text-white hover:bg-blue-600",
+        secondary: "bg-gray-500 text-white hover:bg-gray-600",
+        success: "bg-green-500 text-white hover:bg-green-600",
+        danger: "bg-red-500 text-white hover:bg-red-600",
+        warning: "bg-yellow-500 text-white hover:bg-yellow-600",
+        info: "bg-cyan-500 text-white hover:bg-cyan-600",
+        light: "bg-gray-200 text-gray-800 hover:bg-gray-300",
+        dark: "bg-gray-800 text-white hover:bg-gray-900",
+    }
+    const sizes = {
+        small: "px-2 py-1 text-sm",
+        medium: "px-4 py-2 text-base",
+        large: "px-6 py-3 text-lg",
+    }
+  return (
+   <button className={cn(variants[variant], sizes[size], className)} {...props}>
+     {children}
+   </button>
+  )
+}
+
+export default Button
